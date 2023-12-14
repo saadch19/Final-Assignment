bbdd468eb5db03468d47c4575187323c8a8628ad
saad chaoui <71639978+saadch19@users.noreply.github.com>
2023-12-14 12:27:28 +0100

    Create project.py

---
+++ project.py
@@ -0,0 +1,80 @@
+import yfinance as yf
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+#a function to make a graph from the stock data and revenue (I don't understand it yet).
+def make_graph(stock_data, revenue_data, stock):
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, subplot_titles=("Historical Share Price", "Historical Revenue"), vertical_spacing = .3)
+    stock_data_specific = stock_data[stock_data.Date <= '2021--06-14']
+    revenue_data_specific = revenue_data[revenue_data.Date <= '2021-04-30']
+    fig.add_trace(go.Scatter(x=pd.to_datetime(stock_data_specific.Date, infer_datetime_format=True), y=stock_data_specific.Close.astype("float"), name="Share Price"), row=1, col=1)
+    fig.add_trace(go.Scatter(x=pd.to_datetime(revenue_data_specific.Date, infer_datetime_format=True), y=revenue_data_specific.Revenue.astype("float"), name="Revenue"), row=2, col=1)
+    fig.update_xaxes(title_text="Date", row=1, col=1)
+    fig.update_xaxes(title_text="Date", row=2, col=1)
+    fig.update_yaxes(title_text="Price ($US)", row=1, col=1)
+    fig.update_yaxes(title_text="Revenue ($US Millions)", row=2, col=1)
+    fig.update_layout(showlegend=False,
+    height=900,
+    title=stock,
+    xaxis_rangeslider_visible=True)
+    fig.show()
+
+#Use yfinance to Extract Stock Data:
+Tesla = yf.Ticker("TSLA")
+tesla_data = Tesla.history(period="max")
+tesla_data.reset_index(inplace=True)
+print(tesla_data.head())
+
+
+
+#Use Webscraping to Extract Tesla Revenue Data
+url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/revenue.htm"
+html_data = requests.get(url).text
+soup = BeautifulSoup(html_data, "html5lib")
+data = []   # Initialize an empty list to store data (we're supose to create a dataframe, but something wrong hapens whene we do it).
+
+for row in soup.find_all("tbody")[1].find_all("tr"):
+    col = row.find_all("td")
+    date = col[0].text
+    revenue = col[1].text.replace("$", "").replace(",", "")
+    data.append({"Date": date, "Revenue": revenue})
+
+tesla_revenue = pd.DataFrame(data)  # Create a DataFrame from the list of dictionaries
+tesla_revenue.dropna(inplace=True)
+tesla_revenue = tesla_revenue[tesla_revenue['Revenue'] != ""]
+print(tesla_revenue.tail())
+
+
+#Use yfinance to Extract Stock Data:
+GameStop = yf.Ticker("GME")
+gme_data = GameStop.history(period="max")
+gme_data.reset_index(inplace=True)
+print(gme_data.head())
+
+
+
+#Use Webscraping to Extract GameStop Revenue Data
+url1 = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/stock.html"
+html_data1 = requests.get(url1).text
+soup1 = BeautifulSoup(html_data1, "html5lib")
+data1 = [] # Initialize an empty list to store data (we're supose to create a dataframe, but something wrong hapens whene we do it).
+
+for row in soup1.find_all("tbody")[1].find_all("tr"):
+    col = row.find_all("td")
+    date = col[0].text
+    revenue = col[1].text.replace("$", "").replace(",", "")
+    data1.append({"Date": date, "Revenue": revenue})
+
+gme_revenue = pd.DataFrame(data1)   # Create a DataFrame from the list of dictionaries
+gme_revenue.dropna(inplace=True)
+gme_revenue = gme_revenue[gme_revenue['Revenue'] != ""]
+print(gme_revenue.tail())
+
+
+
+#Creating graphs using the function "make_graph" to present the data and revenue of the stocks.
+make_graph(tesla_data, tesla_revenue, 'Tesla')
+make_graph(gme_data, gme_revenue, 'GameStop')
